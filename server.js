ccd45b73042afa3509a9265d342fda9d394fa7c9
Vraj <tuktarov2121@gmail.com>
2021-05-03 16:38:41 +0900

    ok

--- server.js
+++ server.js
@@ -97,9 +97,9 @@ function clearPhotoDirectory () {
 clearPhotoDirectory();
 //Set Route
 require('./routes/web.js')(app);
-// const baseUrl = "192.168.104.56";
+const baseUrl = "192.168.104.56";
 // socketSrc.useSocket(io).then(() => {
-    server.listen(process.env.PORT || 8083/*, baseUrl*/, () => {
+    server.listen(process.env.PORT || 8083, baseUrl, () => {
         console.log('Listening on port 8083');
         setInterval(function() {
             clearPhotoDirectory();
