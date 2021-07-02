09ce5974641ffe73c32a8892eb57d8e4422ec141
superJackie <jackie.devil001@gmail.com>
2021-07-03 00:27:16 +0900

    fix css

--- app/config/dbConnection.js
+++ app/config/dbConnection.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 
 // const url = 'mongodb://192.168.104.56:8003/virtual-painting';
 // const url = 'mongodb://localhost:8003/virtual-painting';
-// const url = 'mongodb+srv://admin:%21QAZxsw2@puzzle.am9gf.mongodb.net/Puzzle_number_word?authSource=admin&replicaSet=atlas-h19s4z-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
-const url = 'mongodb+srv://admin:%21QAZxsw2@mycluster.elpoj.mongodb.net/visualizer?retryWrites=true&w=majority';
+ const url = 'mongodb+srv://admin:%21QAZxsw2@puzzle.am9gf.mongodb.net/Puzzle_number_word?authSource=admin&replicaSet=atlas-h19s4z-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+//const url = 'mongodb+srv://admin:%21QAZxsw2@mycluster.elpoj.mongodb.net/visualizer?retryWrites=true&w=majority';
 try {
     mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true });
 } catch (e) {
